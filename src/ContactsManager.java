b0115aba6de92ebdb1c22f585c5f74e0c72746e9
Armando Sifuentes <armandosifuentesjr45@gmail.com>
2018-08-07 15:04:44 -0500

    added constructors

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -1,3 +1,14 @@
+import java.util.ArrayList;
+
 public class ContactsManager {
+
+    private String name;
+    private String phoneNumber; //string for numbers to accomodate the dash.
+    private ArrayList<String> ContactsList = new ArrayList<>();
+
+
+
+
+
 
 }
